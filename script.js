1a0d6660fef23e87a64da1675aec4c6c4859175c
haya <mabada0000@gmail.com>
2025-07-15 15:28:16 -0400

    edit index and script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Future Code 2 GitHub Practice File
 
 console.log('Welcome to GitHub practice!');
-console.log("I think Katy is keeping me honest and I'm here for it!!");
+console.log("my name is mohamed !!");
